65b0b60f3fb7e208ef387116a916ea65ac8235bc
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-03-22 14:00:28 +0200

    initial commit

---
+++ app/src/main/java/com/example/tele2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tele2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+    }
+}
